6e61f2c9ed2af783a8dd3d55f438686cd9aa2c10
João Paulo Mazzo <mazzojp@gmail.com>
2024-03-16 10:07:57 -0300

    setting up design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ["./index.html", "./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "hsl(0, 0%, 8%)",
+        green: "hsl(153, 71%, 59%)",
+        darkGrey: "hsl(0, 0%, 14%)",
+        grey: "hsl(0, 0%, 85%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        SpaceGrotesk: ["Space Grotesk", "sans-serif"],
+      },
+      fontSize: {
+        headerXL: "88px",
+        headerL: "48px",
+        headerM: "24px",
+        bodyL: "18px",
+      },
+      letterSpacing: {
+        headerXL: "-2.5px",
+        headerL: "-1.5px",
+      },
+      lineHeight: {
+        headerXL: "88px",
+        headerL: "56px",
+        headerM: "32px",
+        bodyL: "28px",
+      },
+    },
   },
   plugins: [],
 };
