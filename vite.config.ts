f8cc27218ff0175bbef51de8bf2705748e2c07dd
João Paulo Mazzo <mazzojp@gmail.com>
2024-07-25 15:12:55 -0300

    deploy on gh_pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/joaopmazzo",
+  base: "/portifolio",
   plugins: [react(), tsconfigPaths()],
 });
